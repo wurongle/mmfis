6c8a8d8925f231040ceb8cd60b7a1f1c075c272c
wurongle <wurongle@gmail.com>
2014-09-10 22:17:43 +0800

    fix

--- mmfis.js
+++ mmfis.js
@@ -12,16 +12,29 @@ fis.config.set('templates','/template');
 fis.config.set('modules.parser.less','less');
 fis.config.set('roadmap.ext.less','css');
 
-var includeFiles = [];
+var includeFiles = {};
+var includeRegx = /<%#include\(([^#][a-zA-Z0-9-_#.\/:]+)\)%>/ig;
+var setVarIncludeRegx = /<<<subpath>>><<<([^#][a-zA-Z0-9-_#.\/:]+)>>><<<abspath>>><<<([^#][a-zA-Z0-9-_#.\/:]+)>>>/ig;
 
 fis.config.set('modules.preprocessor.html',function (content, file, conf) {
     //console.log(file.filename);
-    content = content.replace(/<%#include\(([^#][a-zA-Z0-9-_#.\/]+)\)%>/g,function (includeStr,url) {
+    content = content.replace(includeRegx,function (includeStr,url) {
+        return '<<<include>>><img src="'+url+'"/>';
+    });
+    return content;
+});
+/*
+ 1. 相对路径用于计算ret.src.key
+ 2. 绝对路径用于计算运行时相对路径
+ */
+fis.config.set('modules.preprocessor.html',function (content, file, conf) {
+    //console.log(file.filename);
+    content = content.replace(includeRegx,function (includeStr,url) {
         //console.log(includeStr,url);
         //console.log(file.subpath);
-        var _url = path.resolve(path.dirname(file.subpath),url).substring(2).replace(/\\/g,'/');
-        return '<%#include('+_url+')%>';
-        //return '<<<include>>><img src="'+url+'"/>';
+        var _url = path.resolve(path.dirname(file.subpath),url).replace(/^\d:/,'').replace(/\\/g,'/');
+        //return '<%#include('+_url+')%>';
+        return '<<<include>>><img subpath="'+_url+'" src="'+url+'"/>';
     });
     return content;
 });
@@ -38,42 +51,76 @@ fis.config.set('modules.postprocessor.html',function (content, file, conf) {
         file.release = file.release.replace(/(\/|__|\.)utf8\./i,'$1');
     }
     /* qqmailtemplate */
-    content = content.replace(/<<<include>>><img src="([^#][a-zA-Z0-9-_#.\/]+)"\/>/g,function (includeStr,url) {
+    content = content.replace(/<<<include>>><img subpath="([^#][a-zA-Z0-9-_#.\/]+)" src="([^#][a-zA-Z0-9-_#.\/]+)"\/>/g,function (includeStr,subpath,abspath) {
         //var _rel = path.relative(path.dirname(file.release),path.dirname(url)) + '/' +path.basename(url);
         //url = url.replace(/(\/|__|\.)(gbk|utf8)\./i,'$1');
         //console.log(file.filename);
-        return '<%#include('+fis.util(url)+')%>';
+        //console.log(file.release,url,path.relative(path.dirname(file.release),url));
+        //var _url = path.resolve(path.dirname(file.release),url).replace(/^\d:/,'').replace(/\\/g,'/');
+        //return '<%#include('+_url+')%>';
+        return "<<<subpath>>><<<"+subpath+">>><<<abspath>>><<<"+abspath+">>>"
+        //return '<%#include('+fis.util(url)+')%>';
     });
     return content;
 });
 
 fis.config.set('modules.prepackager',function (ret, conf, settings, opt) {
     //console.log(ret);
-    console.log("includeFiles",includeFiles);
-    fis.util.map(includeFiles,function (index,file) {
-        console.log('includeFileItem.url',file.url);
+    //console.log("includeFiles",includeFiles);
+    fis.util.map(includeFiles,function (subpath,includes) {
+        var file = ret.src[subpath];
+        var getIncludes = function (include) {
+            var _includes = {};
+            var _fn = function(include) {
+                if(includeFiles[include]){
+                    fis.util.map(includeFiles[include],function (_include,value) {
+                        _includes[_include] = value;
+                        return _fn(_include);
+                    })
+                }else{
+                    _includes[include];
+                }
+            };
+            _fn(include);
+            return _includes;
+        };
+        var setIncludeVars = function (obj) {
+            var setVarsArr = [];
+            fis.util.map(obj,function (item,value) {
+                //console.log(item,value);
+                //console.log("\nSetVar",item,subpath,path.relative(path.dirname(subpath),path.dirname(item)));
+                var _rel = path.relative(path.dirname(file.release),path.dirname(value));
+                _rel += (_rel ? '/' :'./') + path.basename(value); 
+                setVarsArr.push("<%@SetVar("+item+','+_rel+")%>\n");
+            });
+            return setVarsArr.join('');
+        }
+        if(!file){
+            console.log('error, no subpath in src:',subpath);
+        }else{
+            //console.log('getIncludes',getIncludes(subpath));
+            file = fis.file.wrap(file);
+            var _includes = getIncludes(subpath);
+            var _content = setIncludeVars(_includes) + file.getContent().replace(setVarIncludeRegx,function (includeStr,subpath,abspath) {
+                return '<%#include(GetVar('+subpath+'))%>';
+            });
+            file.setContent(_content);
+        }
+
     });
 });
-
+//建立所有文件引用树
 fis.emitter.on('compile:end', function (file) {
-    
-    /*var cache = fis.cache("D:/mmrb_new/projects/mmportal/src/old/gbk.weixin_faq_layout.html", 'compile/release');
-    revertObj = {};
-    cache.revert(revertObj)
-    //revertObj.content = revertObj.content.toString('utf8');
-    console.log(revertObj.content);*/
 
     if(file.ext == ".html"){
         //console.log(file);
-        var includeFileItem = {
-            includes:{}
-        };
-        file._content.replace(/<%#include\(([^#][a-zA-Z0-9-_#.\/:]+)\)%>/ig,function (includeStr,url) {
-            includeFileItem.url = file.url;
-            var _url = url.replace(/#.*/,'');
-            includeFileItem.includes[_url] = true;
+        file._content.replace(setVarIncludeRegx,function (includeStr,subpath,abspath) {
+            subpath = subpath.replace(/#.*/,'');
+            if(!includeFiles[file.subpath]){
+                includeFiles[file.subpath] = {};
+            }
+            includeFiles[file.subpath][subpath] = abspath;
         });
-        includeFileItem.url && includeFiles.push(includeFileItem);
     }
 
 });
